105fdd7dbc287bafdb24fb7f4e7a26f2179c4aa5
exeuSeer <underground.x5@yandex.ru>
2019-01-14 21:28:17 +0300

    create of component for adding tasks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <div id="app">
+    <header>Todo</header>
+    <AddTask @add-task="addTask"></AddTask>
   </div>
 </template>
 
 <script>
+import AddTask from './components/AddTask.vue'
 
 export default {
   name: 'App',
-  components: {}
+  data: function () {
+    return {
+      tasks: []
+    }
+  },
+  components: {
+    AddTask
+  },
+  methods: {
+    addTask (title) {
+      this.tasks.push({title: title, completed: false});
+    }
+  },
 }
 </script>
 
 <style>
-
+  body {
+    margin-left: 30%;
+  }
+  header {
+    font-size: 8em;
+    text-shadow: 1px 1px 2px black, 0 0 1em green;
+  }
+  #app {
+    max-width: 300px;
+  }
 </style>
---
+++ src/components/AddTask.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div class="add-task">
+    <input v-model="title" autofocus/>
+    <button @click="addTask">add new task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddTask',
+  data: function () {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addTask () {
+      this.$emit('add-task', this.title);
+      this.title = '';
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  .add-task input {
+    display: inline-table;
+    border-radius: 10px;
+    height: 2em;
+    width: 100%;
+  }
+  .add-task button {
+    height: 2em;
+    border: 0;
+    border-radius: 5px;
+    background-color: #3CB371;
+  }
+</style>
